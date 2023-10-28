36d5cab84135b4a88472d5ef9e5f3592bfd5469d
OlivierMaria <olivier-maria@hotmail.com>
2023-10-28 11:25:44 +0200

    Addition of the toggle menu in the navbar when responsive and removal of sign in sign up button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.png";
 import "./navbar.css";
 
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="web3wise__navbar">
       <div className="web3wise__navbar-links">
@@ -28,9 +30,45 @@ function Navbar() {
           </p>
         </div>
       </div>
-      <div className="web3wise__navbar-sign">
-        <p>Nous rejoindre</p>
-        <button type="button">Se connecter</button>
+      <div className="web3wise__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="web3wise__navbar-menu_container bounce-in-top">
+            <div className="web3wise_navbar-menu_container-links">
+              <p>
+                <a href="#">Accueil</a>
+              </p>
+              <p>
+                <a href="#">Le Web3, qu'est-ce que c'est ?</a>
+              </p>
+              <p>
+                <a href="#">Technologies Web3</a>
+              </p>
+              <p>
+                <a href="#">Projets Web3</a>
+              </p>
+              <p>
+                <a href="#">Actualités Web3</a>
+              </p>
+              <div className="web3wise__navbar-menu_container-links-sign">
+                <p>Nous rejoindre</p>
+                <button type="button">Se connecter</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
